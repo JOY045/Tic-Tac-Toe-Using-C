5d3f4ed80d008e7423ef2d54802709299973d749
JOY045 <shoeb.mahmud0099@gmail.com>
2022-07-04 02:29:27 +0600

    tic tac toe

---
+++ TicTacTtoe.c
@@ -0,0 +1,139 @@
+/* Shoeb Mahmud Joy
+ID: 192-15-13045
+Daffodil International University */
+
+
+#include <stdio.h>
+#include <conio.h>
+#include<windows.h>
+
+char box[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+int check();
+void board();
+
+int main()
+{
+    int player = 1, i, choice;
+
+    char mark;
+    do
+    {
+        board();
+        player = (player % 2) ? 1 : 2;
+
+        printf("Player %d, Enter A Number:  ", player);
+        scanf("%d", &choice);
+
+        mark = (player == 1) ? 'X' : 'O';
+
+        if (choice == 1 && box[1] == '1')
+            box[1] = mark;
+
+        else if (choice == 2 && box[2] == '2')
+            box[2] = mark;
+
+        else if (choice == 3 && box[3] == '3')
+            box[3] = mark;
+
+        else if (choice == 4 && box[4] == '4')
+            box[4] = mark;
+
+        else if (choice == 5 && box[5] == '5')
+            box[5] = mark;
+
+        else if (choice == 6 && box[6] == '6')
+            box[6] = mark;
+
+        else if (choice == 7 && box[7] == '7')
+            box[7] = mark;
+
+        else if (choice == 8 && box[8] == '8')
+            box[8] = mark;
+
+        else if (choice == 9 && box[9] == '9')
+            box[9] = mark;
+
+        else
+        {
+            printf("Invalid Move");
+
+            player--;
+            getch();
+        }
+        i = check();
+
+        player++;
+    }
+    while (i ==  - 1);
+
+    board();
+
+    if (i == 1)
+        printf("==>\aPlayer %d Win", --player);
+    else
+        printf("==>\aGame Draw");
+
+    getch();
+
+    return 0;
+}
+
+int check()
+{
+    if (box[1] == box[2] && box[2] == box[3])
+        return 1;
+
+    else if (box[4] == box[5] && box[5] == box[6])
+        return 1;
+
+    else if (box[7] == box[8] && box[8] == box[9])
+        return 1;
+
+    else if (box[1] == box[4] && box[4] == box[7])
+        return 1;
+
+    else if (box[2] == box[5] && box[5] == box[8])
+        return 1;
+
+    else if (box[3] == box[6] && box[6] == box[9])
+        return 1;
+
+    else if (box[1] == box[5] && box[5] == box[9])
+        return 1;
+
+    else if (box[3] == box[5] && box[5] == box[7])
+        return 1;
+
+    else if (box[1] != '1' && box[2] != '2' && box[3] != '3' &&
+             box[4] != '4' && box[5] != '5' && box[6] != '6' && box[7]
+             != '7' && box[8] != '8' && box[9] != '9')
+
+        return 0;
+    else
+        return  - 1;
+}
+
+void board()
+{
+    system("cls");
+    printf("\n\n\tTic Tac Toe\n\n");
+
+    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+
+
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", box[1], box[2], box[3]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", box[4], box[5], box[6]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", box[7], box[8], box[9]);
+
+    printf("     |     |     \n\n");
+}
